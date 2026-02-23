03418f2025352684329622191a3ce9db3de9026f
Niko-Stoner <47877148+Niko-Stoner@users.noreply.github.com>
2026-02-23 17:23:01 +1100

    Add files via upload

---
+++ hello world.py
@@ -0,0 +1,18 @@
+"""
+1.2PP Hello World
+"""
+
+__author__ = "Niko"
+
+
+def main():
+    print("Crazy?")
+    print("I was crazy once")
+    print("they locked me in a room")
+    print("a rubber room")
+    print("a rubber room with rats")
+    print("and rats make me crazy")
+
+
+if __name__ == "__main__":
+    main()
