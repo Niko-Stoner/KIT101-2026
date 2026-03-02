5f206b0016b59fe2d0e643b8b0743f0d7f9d91bb
Niko <stonerniko1@gmail.com>
2026-03-02 14:19:23 +1100

    new weeks work

---
+++ 2.1PP Turtle Graphics/initials.py
@@ -0,0 +1,30 @@
+"""
+KIT101 2.1PP Turtle Graphics
+
+Turtle Graphics task to draw the author's initials.
+Some of the code below has been _over_ commented to help
+you understand what is happening.
+"""
+
+__author__ = "YOUR NAME"
+
+import turtle as painter
+
+
+def main():
+    # Change turtle speed if desired
+    # (1=slowest .. 10=fastest | 0=no animation)
+    painter.speed(3)
+
+    # Draw your initials below, remembering to use painter.penup() to
+    # move without drawing a line...
+
+
+
+
+    # Avoid closing the window automatically
+    painter.mainloop()
+
+
+if __name__ == "__main__":
+    main()
