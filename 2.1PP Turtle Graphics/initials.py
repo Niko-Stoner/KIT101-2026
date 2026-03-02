fd7aceb2349871f682d836b74fce5edcc55cad55
Niko <stonerniko1@gmail.com>
2026-03-02 16:04:27 +1100

    code is done

--- 2.1PP Turtle Graphics/initials.py
+++ 2.1PP Turtle Graphics/initials.py
@@ -8,24 +8,58 @@ you understand what is happening.
 
 __author__ = "YOUR NAME"
 
-import turtle as painter
+import turtle as t
 
 
 def main():
     # Change turtle speed if desired
     # (1=slowest .. 10=fastest | 0=no animation)
-    painter.speed(3)
+    t.speed(3)
 
     # Draw your initials below, remembering to use painter.penup() to
     # move without drawing a line...
     t.penup()
     t.pensize(10)
+    t.pencolor("magenta")
     t.left(135)
+    t.forward(200)
+    t.right(45)
+    t.pendown()
+    t.forward (120)
+    t.right(150)
+    t.forward(140)
+    t.left(150)
+    t.forward(120)
+    t.penup()
+    t.right(90)
+    t.forward(300)
+    t.right(180)
+    t.pendown()
+    t.pencolor("cyan")
+    t.forward(60)
+    t.left(90)
+    t.forward(60)
+    t.left(90)
+    t.forward(60)
+    t.right(90)
+    t.forward(60)
+    t.right(90)
+    t.forward(60)
+    t.penup()
+    t.left(45)
+    t.forward(300)
+    t.pendown()
+    t.pencolor("green")
+    degree = 360
+    while degree >0:
+        t.forward(1)
+        t.left(1)
+        degree -= 1
 
 
 
     # Avoid closing the window automatically
-    painter.mainloop()
+    t.mainloop()
 
 
 if __name__ == "__main__":
