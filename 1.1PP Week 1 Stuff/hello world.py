80d531988bbe33485d5a2105c53a917a13c8bdf6
Niko <stonerniko1@gmail.com>
2026-03-02 14:37:37 +1100

    moved files to proper folders

---
+++ 1.1PP Week 1 Stuff/hello world.py
@@ -0,0 +1,24 @@
+"""
+1.2PP Hello World
+"""
+
+__author__ = "Niko"
+
+
+def main():
+    print("Crazy?")
+    print("I was crazy once")
+    print("they locked me in a room")
+    print("a rubber room")
+    print("a rubber room with rats")
+    print("and rats make me crazy")
+    print("tessssst")
+    print("holy shit")
+    print("does this one work at least?")
+    print("i think i've got it figured out, or at least a weird workaround")
+    print("wait this might be easier than i thought ")
+    print("y'know printing is getting pretty dull")
+
+
+if __name__ == "__main__":
+    main()
