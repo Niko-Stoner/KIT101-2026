4b349d839e1fc1c668c73fb1515563bac415a516
Niko <stonerniko1@gmail.com>
2026-03-30 14:54:34 +1100

    yo it actually works now

---
+++ 3.2 Fill in the Blanks/tourdefrance.py
@@ -0,0 +1,18 @@
+"""
+3.2PP Fill in the Blanks: Tour de France Wrap-up
+"""
+
+__author__ = "YOUR NAME"
+
+
+def main():
+    print("Tour de France 2025 – Final Standings")
+    print()
+
+    first_place: str = input("Enter the name of the rider in first place: ")
+    country_1: str = input("What country is the first-placed rider from? ") 
+    total_time_1: float = float(input("Enter the total time (in hours) of the first-placed rider: "))
+
+
+if __name__ == "__main__":
+    main()
