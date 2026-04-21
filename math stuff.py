8144ce22c46e20fbded982f9aebb7754da6f1a2f
Niko <stonerniko1@gmail.com>
2026-04-21 12:30:36 +1000

    finally got ts done

---
+++ math stuff.py
@@ -0,0 +1,26 @@
+import math
+
+##b = 9
+##c = 4.5
+##d = b//c
+##print(d)
+##e= "R"
+##print("Socce" + e)
+##a = 28
+##f = a + 6 *b
+##print(f)
+i = 6
+c = "q"
+s = "cat"
+combined = f"{i}{s}{c}{i}"
+
+print("Result is: ", combined)
+for bar in range(15, 26):
+    print(bar)
+bar = 5
+print("position 1")
+while bar <= 15:
+    print("Position 2")
+    bar += 1
+    print(bar)
+print("position 3")
