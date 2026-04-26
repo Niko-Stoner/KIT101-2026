e1dda018bb58029f0a2d35254044f196f21a344b
Niko <stonerniko1@gmail.com>
2026-04-26 18:06:52 +1000

    new task

---
+++ 5.2 Collection of Strings/words.py
@@ -0,0 +1,32 @@
+"""
+5.2PP Collection of Strings
+"""
+
+__author__ = "YOUR NAME"
+
+
+def add_word(word_list: list[str], word: str):
+    """
+    Adds a new word to the word list if there is capacity and
+    the word is not empty.
+    """
+    # TODO: Complete the implementation of this function
+    ...
+
+
+def display_entries(word_list: list[str]):
+    """
+    Displays all words in the word list, separated by a comma.
+    """
+    # TODO: Complete the implementation of this function
+    ...
+
+
+def main():
+    # TODO: Add necessary variable declarations and initialisations
+    # TODO: Implement the menu
+    ...
+
+
+if __name__ == "__main__":
+    main()
